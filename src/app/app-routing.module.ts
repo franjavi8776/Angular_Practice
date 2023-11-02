0191aa21a2bb336068358853ac0de38996248563
franjavi8776 <franjavi871976@gmail.com>
2023-11-02 17:25:52 -0400

    practice done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductsComponent } from './pages/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact',
+    title: 'Contacto',
+    component: ContactComponent,
+  },
+  {
+    path: 'products',
+    title: 'Productos',
+    component: ProductsComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
